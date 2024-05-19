0051f40d9993db560d29a561a04ac900e17d9234
Vladlislav Leonchyk <u.liavonchyk@tontilagunamobile.com>
2024-05-19 13:49:22 +0200

    chore: Refactor middleware to redirect /chat route to home workspace

--- middleware.ts
+++ middleware.ts
@@ -11,33 +11,12 @@ export async function middleware(request: NextRequest) {
   try {
     const { supabase, response } = createClient(request)
 
-    const { data: { session } } = await supabase.auth.getSession()
+    const session = (await supabase.auth.getSession()).data.session
 
     const redirectToChat = session && request.nextUrl.pathname === "/"
 
     if (redirectToChat) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-      const { data: homeWorkspace, error } = await supabase
-        .from("workspaces")
-        .select("*")
-        .eq("user_id", session.data.session?.user.id)
-        .eq("is_home", true)
-        .single()
-
-      if (!homeWorkspace) {
-        throw new Error(error?.message)
-      }
-
-      return NextResponse.redirect(
-        new URL(`/${homeWorkspace.id}/chat`, request.url)
-      )
-=======
-      return NextResponse.redirect(new URL("/chat", publicUrl(request)))
->>>>>>> c758de0 (Squash org branch over latest main, without some of the more conflicting changes.)
-=======
       return NextResponse.redirect(new URL(`/chat`, publicUrl(request)));
->>>>>>> b205f74 (Add backwards compatible /chat route that redirects to the home workspace)
     }
 
     return response
